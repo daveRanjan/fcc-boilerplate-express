664c2487b858aabfbc11c6f9da0e3d9771cc2c8b
Devendra Tiwari <dtiwari18@gmail.com>
2022-06-08 17:34:19 +0530

    Added myapp.js

--- myApp.js
+++ myApp.js
@@ -36,6 +36,13 @@ app.get('/:word/echo', (req, res) => {
     res.json({echo: req.params.word});
 })
 
+app.route('/name')
+.get((req, res) => {
+    res.json({name: `${req.query.first} ${req.query.last}`})
+})
+.post((req, res) => {
+    res.json({name: `${req.query.first} ${req.query.last}`})
+})
 
 
 
