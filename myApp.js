16edd6dbf2656c52eca41a87c9292d688ee7f33a
Devendra Tiwari <dtiwari18@gmail.com>
2022-06-08 16:13:02 +0530

    Added myapp.js

--- myApp.js
+++ myApp.js
@@ -2,10 +2,11 @@ let express = require('express');
 let app = express();
 
 console.log("Hello World");
+absolutePath = __dirname + '/views/index.html'
 
 
 app.get("/", (req, res)=> {
-    res.send("Hello Express");
+    res.sendFile(absolutePath);
 })
 
 
